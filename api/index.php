9213e87c7b5bd5062641c363f2b1f50f9da290e7
thejsj <jorge.silva@thejsj.com>
2013-09-25 00:11:17 -0400

    Restructuring the way Models are created. Adding Rotten Tomatoes API.

---
+++ api/index.php
@@ -0,0 +1,33 @@
+<?php
+
+  $query = $_POST['query'];
+
+  $apikey = 'sgrfykg5tt5cyj7kahhyyb8t';
+
+  if($query != ""){
+    $q = urlencode($query); // make sure to url encode an query parameters
+  }
+  else {
+    $q = urlencode('Toy Story'); // make sure to url encode an query parameters
+  }
+
+  // construct the query with our apikey and the query we want to make
+  $endpoint = 'http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=' . $apikey . '&q=' . $q;
+
+  // setup curl to make a call to the endpoint
+  $session = curl_init($endpoint);
+
+  // indicates that we want the response back
+  curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+
+  // exec curl and get the data back
+  $data = curl_exec($session);
+
+  // remember to close the curl session once we are finished retrieveing the data
+  curl_close($session);
+
+  // decode the json data to make it easier to parse the php
+  // actually, just spit out this JSON!
+  echo ($data);
+
+?>
--- js/app/main.js
+++ js/app/main.js
@@ -3,80 +3,87 @@ require([
     'underscore',
     'backbone',
 ], function($, _, Backbone){
-    
-    /* Person Model
-    **********************/
 
-	Person = Backbone.Model.extend({
-		defaults: {
-            name: 'Fetus',
-            age: 0,
-            child: ''
-        },
-        initialize: function(){
-            this.on("change:name", function(model){
-                var name = model.get("name"); // 'Stewie Griffin'
-            });
-        },
-        adopt: function( newChildsName ){
-            this.set({ child: newChildsName });
-        }
+    $.ajaxSetup({
+        crossDomain: true,
+        dataType: 'jsonp',
     });
-    
-    var people = [];
-    for(var i = 0; i < 2; i++){
-    	people.push( new Person({ age: parseInt(Math.random() * 10)}) );
-    	people[people.length - 1].adopt(parseInt(Math.random() * 10) + 20)
-    }
-
-    setTimeout(function(){
-    	people[parseInt(Math.random() * people.length)].set({name: 'Stewie Griffin'});
-    }, 1000);
 
     /* Post Model
     **********************/
 
     var Post = Backbone.Model.extend({
-        urlRoot: '/~jorgesilva/2013/backbone/data',
         defaults: {
             id: '',
-            type: '',
             title: '',
-            content: '',
-            url: '',
-            date: '',
-            modified: '',
+            mpaa_rating: '',
+            runtime: '',
+            release_dates: '',
+            ratings: '',
+            synopsis: '',
+            posters: '',
+            abridged_cast: '',
+            links: '',
         },
         initialize :  function(){
             // Try to get id here...
-        },
-        parse : function(response, index, i2){
-            console.log(index);
-            console.log(i2);
-            return response.posts[0];  
-        }   
-    });
-
-    var post = new Post({helo: 1});
-
-    // The fetch below will perform GET /user/1
-    // The server should return the id, name and email from the database
-    post.fetch({
-        success: function (data) {
-            console.log("Post #" + post.get("id") + " loaded succsefully.");
         }
     });
 
     /* Collection
     ************************/
 
-    var PostCollection = Backbone.Collection.extend({
-        model: Post
+    var PostCollection;
+    var allPosts;
+
+    PostCollection = Backbone.Collection.extend({
+        model: Post,
     });
 
-    var AllPosts = new PostCollection([ post ]);
+    /* Query Model
+    *********************/
 
-    console.log( AllPosts.models );
+    var QueryPost = Backbone.Model.extend({
+        url: 'http://localhost/~jorgesilva/2013/BackboneTutorial/api/index.php',
+        defaults: {
+            query :'',
+            results : '',
+            url : '',
+        },
+        initialize : function(){
+
+        },
+        parse : function(response){
+            console.log(" * Parsing Query * ");
+            console.log(response.movies);
+            all = [];
+            for(key in response.movies){
+                all.push(new Post(response.movies[key]));
+            }
+            allPosts = new PostCollection(all);
+            console.log(allPosts.models);
+
+            var list_view = new ListPosts();
+
+            return response.movies;
+        }
+    });
+
+    /* The Query 
+    ************************/
+
+    var this_query = {
+        query: 'James Bond',
+    }
+    
+    var initial_query = new QueryPost();
+
+    initial_query.save(this_query, {
+        success: function (initial_query) {
+            console.log(" * initial_query * ");
+            console.log(initial_query);            
+        }
+    })
 
     /* Views 
     ************************/
@@ -85,9 +92,9 @@ require([
         el: '#posts_container',
         template: _.template($("#single_post_template").html()),
         initialize: function(){
-            this.coll = new PostCollection([ post ]);
+            this.coll = allPosts;
             this.listenTo(this.coll, 'reset', this.render);
-            console.log(this.coll.toJSON());
+            //console.log(this.coll.toJSON());
             this.render();
         },
         render: function(){
@@ -104,7 +111,10 @@ require([
     });
 
     setTimeout(function(){
-        var list_view = new ListPosts();
-    }, 500);
+
+    }, 2000);
+
+    /* Utilities 
+    ************************/
 
 });
